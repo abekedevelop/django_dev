40f8827106e3f88ab38f6aeb36d1096437760ce3
Absat_D <ardyusembaev@kassa24.kz>
2020-05-20 16:37:41 +0600

    remove extra models and migrations

--- polls/models/album.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from .musician import Musician
-
-
-class Album(models.Model):
-    artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
-    release_date = models.DateField()
-    num_stars = models.IntegerField()
