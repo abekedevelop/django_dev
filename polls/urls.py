cffa1f2ddf82f306f5c24fa06a5fe25334d0e323
Absat_D <ardyusembaev@kassa24.kz>
2020-05-20 16:37:53 +0600

    remove extra models and migrations

--- polls/urls.py
+++ polls/urls.py
@@ -4,11 +4,5 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-    path('<int:pk>/results', views.ResultsView.as_view(), name='results'),
-    path('<int:question_id>/vote/', views.vote, name='vote'),
-    path('register/', views.register, name='register'),
-    path('registered/', views.registered, name='registered'),
-    path('auth/', views.authenticate, name='auth')
+    # path('', views.IndexView.as_view(), name='index'),
 ]
